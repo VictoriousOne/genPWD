b438fb1499dd00877e2dd25d260c45f8035e58eb
Kerry Long <victory173451@gmail.com>
2021-12-19 20:14:17 -0500

    added html

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,18 @@ var pwdPrompts = {
     pwdLen: window.prompt("How many characters would you like in your password? It must be at least 8 charracdters but no more than 128.")
 }
 
+var btnPWD = document.querySelector("#btnGenPWD");
+var thePWD = document.querySelector("#pwd");
+
 var letters = "abcdefghijklmnopqrstuvwxyz".split("");
-var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var special = "!~#$%&*+=<>@/:;-^{]}[".split("");
 
 var pwdOption;
 var generatedPWD = [];
 var obj = 0;
 
-var randomNumber = function(min, max) {
+var randomNumber = function (min, max) {
     //var value = Math.floor(Math.random() * (max - min + 1));
     var value = Math.floor(Math.random() * max) + 1;
     return value;
@@ -64,46 +67,51 @@ console.log("pwd length is " + pwdPrompts.pwdLen);
     }
 }*/
 
-var i = 1;
-while (i <= parseInt(pwdPrompts.pwdLen)) {
+function createPWD() {
+    var i = 1;
+    while (i <= parseInt(pwdPrompts.pwdLen)) {
 
-    pwdOption = randomNumber(1,4);
-    if (pwdOption === 1) {
-        if (parseInt(pwdPrompts.lowercase)) {
-            obj = randomNumber(0, (letters.length - 1));
-            console.log("pwdOption for lowercase is " + pwdOption);
-            console.log("lowercase is " + letters[obj]);
-            generatedPWD.push(letters[obj]);
-            i++;
+        pwdOption = randomNumber(1, 4);
+        if (pwdOption === 1) {
+            if (parseInt(pwdPrompts.lowercase)) {
+                obj = randomNumber(0, (letters.length - 1));
+                console.log("pwdOption for lowercase is " + pwdOption);
+                console.log("lowercase is " + letters[obj]);
+                generatedPWD.push(letters[obj]);
+                i++;
+            }
+        }
+        else if (pwdOption === 2) {
+            if (parseInt(pwdPrompts.uppercase)) {
+                obj = randomNumber(0, (letters.length - 1));
+                console.log("pwdOption for uppercase is " + pwdOption);
+                console.log("uppercase is " + letters[obj].toUpperCase());
+                generatedPWD.push(letters[obj].toUpperCase());
+                i++;
+            }
+        }
+        else if (pwdOption === 3) {
+            if (parseInt(pwdPrompts.numeric)) {
+                obj = randomNumber(0, (numbers.length - 1));
+                console.log("pwdOption for number is " + pwdOption);
+                console.log("Number is " + numbers[obj]);
+                generatedPWD.push(numbers[obj]);
+                i++;
+            }
+        }
+        else if (pwdOption === 4) {
+            if (parseInt(pwdPrompts.special)) {
+                obj = randomNumber(0, (special.length - 1));
+                console.log("pwdOption for special is " + pwdOption);
+                console.log("special is " + special[obj]);
+                generatedPWD.push(special[obj]);
+                i++;
+            }
         }
     }
-    else if (pwdOption === 2) {
-        if (parseInt(pwdPrompts.uppercase)) {
-            obj = randomNumber(0, (letters.length - 1));
-            console.log("pwdOption for uppercase is " + pwdOption);
-            console.log("uppercase is " + letters[obj].toUpperCase());
-            generatedPWD.push(letters[obj].toUpperCase());
-            i++;
-        }
-    }
-    else if (pwdOption === 3) {
-        if (parseInt(pwdPrompts.numeric)) {
-            obj = randomNumber(0, (numbers.length - 1));
-            console.log("pwdOption for number is " + pwdOption);
-            console.log("Number is " + numbers[obj]);
-            generatedPWD.push(numbers[obj]);
-            i++;
-        }     
-    }
-    else if (pwdOption === 4) {
-        if (parseInt(pwdPrompts.special)) {
-            obj = randomNumber(0, (special.length - 1));
-            console.log("pwdOption for special is " + pwdOption);
-            console.log("special is " + special[obj]);
-            generatedPWD.push(special[obj]);
-            i++;
-        }
-    }
+    thePWD.innerHTML = generatedPWD.join("");
+    btnPWD.disabled = true;
 }
 
-    alert("Yourpassword is " + generatedPWD.join(""));
+//alert("Yourpassword is " + generatedPWD.join(""));
+btnPWD.addEventListener("click", createPWD);
