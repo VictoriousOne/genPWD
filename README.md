184489b3470e0dfc96335d3b4c28438616f4670b
Kerry Long <victory173451@gmail.com>
2021-12-19 23:09:06 -0500

    removed commented out code

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# genPWD
+# Password Generator
+
+# Purpose
+The purpose of the application is to allow the user to generate a password. The user is prompted for the following information:
+
+Include lowercase characters
+Include uppercase characters
+Include numeric characters
+Include special characters
+Length of password
+
+# Installation
+Site URL: https//victoriousone.github.oi/genPWD
+
+# Contributios
+Contributions are welcome as I could not figure out how to make one function for all the prompts. I tried returning  values but they always came back undefined if the function was called recursively.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,4 @@
 
-/*var pwdPrompts = {
-    lowercase: window.prompt("Would you like lowercase characters? Enter 1 for yes, 0 for no"),
-    uppercase: window.prompt("Would you like uppercase characters? Enter 1 for yes, 0for no"),
-    numeric: window.prompt("Would you like numeric characters? Enter 1 for yes, 0 for no"),
-    special: window.prompt("Would you like special characters? Enter 1 for yes, 0 for no"),
-    pwdLen: window.prompt("How many characters would you like in your password? It must be at least 8 charracdters but no more than 128.")
-}*/
 
 var btnPWD = document.querySelector("#btnGenPWD");
 var thePWD = document.querySelector("#pwd");
@@ -25,11 +18,11 @@ var specialChar;
 var pwdLen;
 
 var randomNumber = function (min, max) {
-    //var value = Math.floor(Math.random() * (max - min + 1));
     var value = Math.floor(Math.random() * max) + 1;
     return value;
 }
 
+//Prompt the user for lowercase characters
 function lowerChoice() {
     lowerChar = parseInt(window.prompt("Include lowercase characters? Enter 1 for yes, 0 for no"));
     if ((!(lowerChar === 1) && !(lowerChar === 0)) || (lowerChar === null)) {
@@ -38,6 +31,7 @@ function lowerChoice() {
     }
 }
 
+//Prompt the user for uppercase characters
 function upperChoice() {
     upperChar = parseInt(window.prompt("Include uppercase characters? Enter 1 for yes, 0 for no"));
     if ((!(upperChar  === 1) && !(upperChar  === 0)) || (upperChar  === null)) {
@@ -46,6 +40,7 @@ function upperChoice() {
     }
 }
 
+//Prompt the user for numeric characters
 function numChoice() {
     numChar = parseInt(window.prompt("Include numeric characters? Enter 1 for yes, 0 for no"));
     if ((!(numChar  === 1) && !(numChar  === 0)) || (numChar  === null)) {
@@ -54,6 +49,7 @@ function numChoice() {
     }
 }
 
+//Prompt the user for special characters
 function specialChoice() {
     specialChar = parseInt(window.prompt("Include special characters? Enter 1 for yes, 0 for no"));
     if ((!(specialChar  === 1) && !(specialChar  === 0)) || (specialChar  === null)) {
@@ -62,6 +58,7 @@ function specialChoice() {
     }
 }
 
+//Prompt the user for the password length
 function pwdLenChoice() {
     pwdLen = parseInt(window.prompt("Please enter a number between 8 and 128 for the password length"));
    if (!((pwdLen >= 8) && (pwdLen <= 128)) || (pwdLen === null)) {
@@ -72,52 +69,13 @@ function pwdLenChoice() {
 
 
 console.log("pwd length is " + pwdLen);
-/*for (var i = 0; i < parseInt(pwdPrompts.pwdLen); i++) {
-    pwdOption = randomNumber(1,4);
-    console.log("pwdOption is " + pwdOption);
 
-    switch (pwdOption) {
-        case 1 :
-            if (parseInt(pwdPrompts.lowercase)) {
-                obj = randomNumber(0, (letters.length - 1));
-                console.log("lowercase is " + letters[obj]);
-
-                //generatedPWD.push(letters[obj]);
-                generatedPWD[i] = letters[obj];
-                break;
-            }
-        case 2 :
-            if (parseInt(pwdPrompts.uppercase)) {
-                obj = randomNumber(0, (letters.length - 1));
-                console.log("uppercase is " + letters[obj].toUpperCase());
-                //generatedPWD.push(letters[obj].toUpperCase());
-                generatedPWD[i] = letters[obj].toUpperCase();
-                break;
-            }
-        case 3 :
-            if (parseInt(pwdPrompts.numeric)) {
-                obj = randomNumber(0, (numbers.length - 1));
-                console.log("Number is " + numbers[obj]);
-                //generatedPWD.push(numbers[obj]);
-                generatedPWD[i] = numbers[obj];
-                break;
-            }
-        case 4 : 
-            if (parseInt(pwdPrompts.special)) {
-                obj = randomNumber(0, (special.length - 1));
-                console.log("pwdOption for special is " + pwdOption);
-                console.log("special is " + special[obj]);
-                //generatedPWD.push(special[obj]);
-                generatedPWD[i] = special[obj];
-                break;
-            }
-    }
-}*/
-
+//Create the password
 function createPWD() {
     var i = 1;
     while (i <= parseInt(pwdLen)) {
 
+        //Randomize the user selections
         pwdOption = randomNumber(1, 4);
         if (pwdOption === 1) {
             if (parseInt(lowerChar)) {
@@ -160,6 +118,7 @@ function createPWD() {
     btnPWD.disabled = true;
 }
 
+//Prompt the user for info
 function getUserInfo() {
     lowerChoice();
     upperChoice();
@@ -167,6 +126,7 @@ function getUserInfo() {
     specialChoice();
     pwdLenChoice();
 
+    //Check to see if the user selected at least on option
     if ((lowerChar === 1) || (upperChar == 1) || (numChar === 1) || (specialChar === 1)) {
         alert("You may now select the button to generate your password");
         btnPWD.disabled = false;
@@ -178,6 +138,6 @@ function getUserInfo() {
         
     }
 }
+
+//Get User Info
 getUserInfo();
-//alert("Yourpassword is " + generatedPWD.join(""));
-//btnPWD.addEventListener("click", createPWD);
